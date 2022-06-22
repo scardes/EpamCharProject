56db5dbeb4644209558d32ba9e4ebe68c9d92879
Eldiiar Dyushenaliev <scardes93@gmail.com>
2022-06-22 18:02:04 +0600

    Обернул в метод. + проверка на 1 символ.

--- EpamCharProject/Program.cs
+++ EpamCharProject/Program.cs
@@ -2,58 +2,60 @@
 
 namespace EpamCharProject
 {
-    //Написать программу, которая принимает из командной строки последовательность символов (строку) в качестве аргумента 
+    //Программа, которая принимает из командной строки последовательность символов (строку) в качестве аргумента 
     //и выводит в консоль максимальное количество неодинаковых последовательных символов в строке
     class Program
     {
         static void Main()
         {
-            bool endApp = false; 
+            bool endApp = false;
             
             while (!endApp) //Повторять программу пока не получим ключ "n"
             {
-                Console.WriteLine("Введите строку для поиска в ней последовательность уникальных символов");
+                Console.WriteLine("Enter a string to search for a sequence of unique characters");
                 string? str = Console.ReadLine(); // "?" Вопрос чтобы принимал значение null - при вводе строки
+
+                //string uniqueStringMax = FindUnique(str);
+
+                Console.WriteLine("\nMAX unique symbols is: {0}", FindUnique(str));
+                Console.WriteLine("------------------------\n");
+
+                Console.Write("Press 'n' and Enter to close the app, or press any key and Enter to continue: "); 
+                if (Console.ReadLine() == "n") endApp = true;
+                Console.WriteLine("\n"); // Friendly linespacing
+            }
+            return;
+
+
+            string FindUnique(string inputLine)
+            {
                 string unicStr = "";
                 string unicStrMax = "";
                 string unicStrMaxTemp = "";
-                //int count = 0;
-                //int countMax = 0;
 
-                for (int i = 0; i < (str.Length - 1); i++) //(str.Length-1) - чтобы не выходить за пределы массива 
+                if (inputLine.Length == 1) unicStrMax = inputLine; //Если введен один символ
+
+                for (int i = 0; i < (inputLine.Length - 1); i++) //(str.Length-1) - чтобы не выходить за пределы массива 
                 {
-                    if (str[i] != str[i + 1]) // If symbols NOT is equal //Цикл сравнения символов
+                    if (inputLine[i] != inputLine[i + 1]) // If symbols NOT is equal //Цикл сравнения символов
                     {
-                        Console.WriteLine("DIFF >> {0} и {1} ", str[i], str[i + 1]);
-                        unicStr = unicStr + str[i]; //Записываем текущий символ
-                        unicStrMaxTemp = unicStr + str[i+1]; //Записываем след сравнимаемый символ 
-                        if (unicStrMaxTemp.Length > unicStrMax.Length) // Сравниваем Врем.макс с основным Макс символоми
-                        {
-                            unicStrMax = unicStrMaxTemp; // Переписываем Макс если Темп больше
-                        }
+                        //Console.WriteLine("DIFF >> {0} и {1} ", str[i], str[i + 1]); // For debug 
+                        unicStr += inputLine[i]; //Записываем текущий символ
+                        unicStrMaxTemp = unicStr + inputLine[i + 1]; //Записываем след сравнимаемый символ 
+                        if (unicStrMaxTemp.Length > unicStrMax.Length) unicStrMax = unicStrMaxTemp; // Сравниваем Врем.макс с основным > Макс символоми Переписываем Макс если Темп больше
                     }
+
                     else
                     {
-                        Console.WriteLine("SAME << {0} и {1} ", str[i], str[i + 1]);
-                        // символы похожи сохраняем текущую макс строку и начинаем собирать новую строку
+                        //Console.WriteLine("SAME << {0} и {1} ", str[i], str[i + 1]); // For debug символы похожи сохраняем текущую макс строку и начинаем собирать новую строку
                         unicStrMax = unicStrMaxTemp; //Записываем след сравнимаемый символ 
                         unicStr = "";
                         unicStrMaxTemp = "";
                     }
-                }  
+                }
 
-
-                Console.WriteLine("MAX unic symbols - {0}",  unicStrMax);
-
-                Console.WriteLine("------------------------\n");
-                
-                // Wait for the user to respond before closing.
-                Console.Write("Нажмите 'n' и Enter чтобы выйти из программы, или введите любое значение и Enter для продолжения работы с программой: ");
-                if (Console.ReadLine() == "n") endApp = true;
-
-                Console.WriteLine("\n"); // Friendly linespacing
+                return unicStrMax;
             }
-            return;
         }
     }
 }

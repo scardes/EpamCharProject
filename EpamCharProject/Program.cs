013b17d2494d8c5b8f87a51bd2f62b2e8abb5212
Eldiiar Dyushenaliev <scardes93@gmail.com>
2022-06-27 18:01:45 +0600

    Little English comments

--- EpamCharProject/Program.cs
+++ EpamCharProject/Program.cs
@@ -2,62 +2,84 @@
 
 namespace EpamCharProject
 {
-    //Программа, которая принимает из командной строки последовательность символов (строку) в качестве аргумента 
-    //и выводит в консоль максимальное количество неодинаковых последовательных символов в строке
+    /// <summary>
+    /// Console program which accepts a sequence of characters (string) as an argument and 
+    /// prints the maximum number of different sequential characters into the console
+    /// </summary>
     class Program
     {
         static void Main()
         {
             bool endApp = false;
 
-            while (!endApp) //Повторять программу пока не получим ключ "n"
+            // Repeat program, until input key "n" to exit.
+            while (!endApp) 
             {
                 Console.WriteLine("Enter a string to search for a sequence of unique characters");
-                string? str = Console.ReadLine(); // "?" Вопрос чтобы принимал значение null - при вводе строки
 
-                //string uniqueStringMax = FindUnique(str);
+                // Ask the user to type the string (sequence of characters)
+                string? inputLine = Console.ReadLine(); // "string?" - for nullable string
 
-                Console.WriteLine("\nMAX unique symbols is: {0}", FindUnique(str)); //
+                // Invoke 
+                Console.WriteLine("\nMAX unique symbols is: {0}", FindUnique(inputLine)); 
                 Console.WriteLine("------------------------\n");
 
+                // Wait for the user to respond before closing.
                 Console.Write("Press 'n' and Enter to close the app, or press any key and Enter to continue: ");
-                if (Console.ReadLine() == "n") endApp = true;
+
+                if (Console.ReadLine() == "n")
+                {
+                    endApp = true;
+                } 
+                    
                 Console.WriteLine("\n"); // Friendly linespacing
             }
             return;
 
-
+            
             string FindUnique(string inputLine)
             {
-                string unicStr = "";
-                string unicStrMax = "";
-                string unicStrMaxTemp = "";
+                string uniqueStr = "";
+                string uniqueStrMax = "";
+                string uniqueStrMaxTemp;
 
-                if (inputLine.Length == 1) unicStrMax = inputLine; //Если введен один символ
-
-                for (int i = 0; i < (inputLine.Length - 1); i++) //(str.Length-1) - чтобы не выходить за пределы массива 
+                // If "inputLine" has only one symbol
+                if (inputLine.Length == 1)
                 {
-                    if (inputLine[i] != inputLine[i + 1]) // If symbols NOT is equal //Цикл сравнения символов
-                    {
-                        //Console.WriteLine("DIFF >> {0} и {1} ", inputLine[i], inputLine[i + 1]); // For debug 
-                        unicStr += inputLine[i]; //Записываем текущий символ
-                        unicStrMaxTemp = unicStr + inputLine[i + 1]; //Записываем след сравнимаемый символ 
-                    }
-
-                    else
-                    {
-                        //Console.WriteLine("SAME << {0} и {1} ", inputLine[i], inputLine[i + 1]); // For debug Начинаем собирать новую строку
-                        unicStr = "";
-                        unicStrMaxTemp = "";
-                    }
-
-                    if (unicStrMaxTemp.Length == 0) unicStrMaxTemp += inputLine[0]; // Если все символы одинаковые. 
-
-                    if (unicStrMaxTemp.Length > unicStrMax.Length) unicStrMax = unicStrMaxTemp; // Сравниваем Врем.макс с основным > Макс символоми Переписываем Макс если Темп больше
-                   
+                    uniqueStrMax = inputLine;
                 }
 
-                return unicStrMax;
+                for (int i = 0; i < (inputLine.Length - 1); i++) // (inputLine.Length - 1) - To avoid the Error: 'Index was outside the bounds of the array.'
+                {
+                    // If symbols NOT is equal, safe both symbols
+                    if (inputLine[i] != inputLine[i + 1]) 
+                    {
+                        uniqueStr += inputLine[i]; // Safe current symbol
+                        uniqueStrMaxTemp = uniqueStr + inputLine[i + 1]; // Safe next symbol
+                    }
+
+                    // If symbols is equal: reset collectors
+                    else 
+                    {
+                        uniqueStr = "";
+                        uniqueStrMaxTemp = "";
+                    }
+
+                    // If "inputLine" has only same symbols (like "11111")
+                    if (uniqueStrMaxTemp.Length == 0)
+                    {
+                        uniqueStrMaxTemp += inputLine[0]; 
+                    }
+
+                    // Safe unique max string 
+                    if (uniqueStrMaxTemp.Length > uniqueStrMax.Length)
+                    {
+                        uniqueStrMax = uniqueStrMaxTemp; 
+                    }
+
+                }
+
+                return uniqueStrMax;
             }
         }
     }
